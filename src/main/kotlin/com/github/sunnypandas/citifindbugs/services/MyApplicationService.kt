a9fdd37b2b3ad5bd080ae36e3e3779ccc3c38a54
GitHub Action <action@github.com>
2022-03-22 01:57:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/sunnypandas/citifindbugs/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sunnypandas/citifindbugs/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sunnypandas.citifindbugs.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sunnypandas.citifindbugs.MyBundle
 
 class MyApplicationService {
 
